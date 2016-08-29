a9fd894fd0709022ef02afdab99028535bbb7140
Tim Yu <fishsea@gmail.com>
2016-08-29 11:52:05 +0800

    add tutorial template

--- routes.js
+++ routes.js
@@ -5,13 +5,8 @@ var router = express.Router();
 /*=====
  SETUP
 ======*/
-var braintree = require('braintree');
-var gateway = braintree.connect({
-    environment:  braintree.Environment.Sandbox,
-    merchantId:   process.env.BT_MERCHANT_ID,
-    publicKey:    process.env.BT_PUBLIC_KEY,
-    privateKey:   process.env.BT_PRIVATE_KEY
-});
+
+// set up Braintree
 
 router.get('/', function (req, res) {
     res.redirect('index.html');
@@ -21,26 +16,15 @@ router.get('/', function (req, res) {
  GET TOKEN
 ===========*/
 router.get('/get_token', function (req,res) {
-    gateway.clientToken.generate({}, function(err, response) {
-        res.send(response);
-    });
+    // generate client token
+
 });
 
 /*===============
  PROCESS PAYMENT
 ================*/
 router.post('/pay', function (req, res) {
-    var nonce = req.body.payment_method_nonce;
-console.log(nonce);
-    gateway.transaction.sale({
-        amount: '3.50',
-        paymentMethodNonce: nonce
-    }, function (err, result) {
-        if (result.success) {
-            console.log(result);
-            res.redirect('success.html');
-        }
-    });
+    // create a transaction
 });
 
 module.exports = router;
